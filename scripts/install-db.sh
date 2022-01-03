12caa09fc9e8d2ab4744a5513022f5531b048ff9
Vincent Zhong <vincentzhongca@gmail.com>
2022-01-02 19:23:41 -0800

    Add db scripts

---
+++ scripts/install-db.sh
@@ -0,0 +1,26 @@
+#! /bin/bash
+set -ex
+
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+# read config
+source $SCRIPT_DIR/cluster-config.sh
+
+mkdir -p $DATA_PATH
+mkdir -p $ARCHIVE_PATH
+
+initdb $DATA_PATH
+
+sed -i -e "s|^#port.*|port=$PORT|" $PG_CONFIG
+sed -i -e 's|^#hot_standby.*|hot_standby= on|' $PG_CONFIG
+sed -i -e "s|^#listen_addresses.*|listen_addresses= '*'|" $PG_CONFIG
+sed -i -e "s|^#unix_socket_directories.*|unix_socket_directories= '/tmp'|" $PG_CONFIG
+sed -i -e "s|^#archive_command.*|archive_command= 'scp %p clzhong@$REPLICA:~/$ARCHIVE_PATH/%f'|" $PG_CONFIG
+sed -i -e "s|^#primary_conninfo.*|primary_conninfo= 'host=$REPLICA port=$PORT dbname=$DB_NAME user=rep_user connect_timeout=10'|" $PG_CONFIG
+
+echo 'host    replication     rep_user        192.168.1.1/24            trust' >> $PG_HBA
+echo 'host    all             all             192.168.1.1/24            md5' >> $PG_HBA
+
+pg_ctl -D /home/clzhong/db/DATA -l logfile start
+psql -h /tmp postgres -p $PORT -c "CREATE ROLE rep_user with PASSWORD '$PASSWORD' LOGIN REPLICATION;"
+psql -h /tmp postgres -p $PORT -c "CREATE ROLE admin_user with PASSWORD '$PASSWORD' LOGIN;"
+psql -h /tmp postgres -p $PORT -c "CREATE DATABASE $DB_NAME with OWNER='admin_user';"
---
+++ scripts/install-replica.sh
@@ -0,0 +1,25 @@
+#! /bin/bash
+set -ex
+
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+# read config
+source $SCRIPT_DIR/cluster-config.sh
+
+mkdir -p $DATA_PATH
+mkdir -p $ARCHIVE_PATH
+
+pg_basebackup -D $DATA_PATH --wal-method=stream -p -h $PRIMARY -p $PORT -U rep_user -W $PASSWORD
+
+sed -i -e "s|^#port.*|port=$PORT|" $PG_CONFIG
+sed -i -e 's|^#hot_standby.*|hot_standby= on|' $PG_CONFIG
+sed -i -e "s|^#listen_addresses.*|listen_addresses= '*'|" $PG_CONFIG
+sed -i -e "s|^#unix_socket_directories.*|unix_socket_directories= '/tmp'|" $PG_CONFIG
+sed -i -e "s|^#archive_command.*|archive_command= 'scp %p clzhong@$PRIMARY:~/$ARCHIVE_PATH/%f'|" $PG_CONFIG
+sed -i -e "s|^#primary_conninfo.*|primary_conninfo= 'host=$PRIMARY port=$PORT dbname=$DB_NAME user=rep_user connect_timeout=10'|" $PG_CONFIG
+
+touch $DATA_PATH/standby.signal
+
+echo 'host    replication     rep_user        192.168.1.1/24            trust' >> $PG_HBA
+echo 'host    all             all             192.168.1.1/24            md5' >> $PG_HBA
+
+pg_ctl -D /home/clzhong/db/DATA -l logfile start
